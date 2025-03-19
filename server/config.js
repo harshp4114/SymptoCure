3d93299111fd2f6592d0bd10c28e5e2ef31e079c
harshp4114 <harshpatadia4114@gmail.com>
2025-03-20 00:35:29 +0530

    updated json to deploy

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function connectMongo() {
   await mongoose
-    .connect("mongodb+srv://harshpatadia4114:harshp@@@@4114@symptocure.criql.mongodb.net/")
+    .connect("mongodb+srv://harshpatadia4114:harshp@@@@4114@symptocure.criql.mongodb.net/symptocure?retryWrites=true&w=majority")
     .then(() => {
       //console.log("srever connected to mongo successfully");
     })
