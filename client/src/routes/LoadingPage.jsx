4de176549732b43b9b24e87adfddae6e9c4f22fd
harshp4114 <harshpatadia4114@gmail.com>
2025-03-20 01:24:41 +0530

    deployed. fixing loader build issue

--- client/src/routes/LoadingPage.jsx
+++ client/src/routes/LoadingPage.jsx
@@ -1,13 +1,19 @@
 import "ldrs/helix"; // Ensure this path is valid
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LoadingPage = () => {
-    
-    const navigate=useNavigate();
-    setTimeout(()=>{
-        navigate("/home");
-    },1500);
-    return (
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    import("ldrs").then((ldrs) => {
+      ldrs.register(); // Ensure the loader components are registered
+    });
+    setTimeout(() => {
+      navigate("/home");
+    }, 1500);
+  },[navigate]);
+  return (
     <div className=" w-full h-full flex justify-center bg-[#16165C] items-center">
       <l-helix size="165" speed="1" color="white"></l-helix>
     </div>
