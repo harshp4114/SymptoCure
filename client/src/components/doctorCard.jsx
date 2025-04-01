4157ee60d191afc0bc33704e169fe76ffc43e83d
harshp4114 <harshpatadia4114@gmail.com>
2025-04-01 16:57:48 +0530

    ui changes

--- client/src/components/doctorCard.jsx
+++ client/src/components/doctorCard.jsx
@@ -8,10 +8,12 @@ const DoctorCard = (props) => {
       {/* {console.log("hiiii",props.data)} */}
       <div className="w-64 h-[24rem] justify-center content-start items-start m-4 ring-[5px] ring-blue-300 hover:ring-[10px] bg-white p-0 transition-all duration-500 ease-in-out rounded-lg flex flex-wrap">
         <div className="w-full h-52">
-          <img
-            src={props.image}
-            className="h-full w-full rounded-t-lg opacity-95 object-cover"
-          ></img> 
+          <div className="w-full h-full flex justify-center items-center bg-[#072965]">
+            <h2 className="text-5xl font-bold">
+              {props?.data?.fullName?.firstName.slice(0, 1).toUpperCase() +
+                props?.data?.fullName?.lastName.slice(0, 1).toUpperCase()}
+            </h2>
+          </div>
         </div>
         <div className="flex flex-wrap justify-start mt-2 items-start w-full mx-3">
           <h2 className="font-bold text-xl mb-2">
